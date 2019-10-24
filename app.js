af2723570cbaccdc1c542a7e6b4bc0f4c4049611
Mike Spadafora <miikekm@gmail.com>
2019-10-23 23:21:54 -0400

    removed formidable middleware

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(formidableMiddleware)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
