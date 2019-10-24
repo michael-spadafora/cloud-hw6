3e6b9d5a95096f9ad73dcd44ccca1ed01a605945
Mike Spadafora <miikekm@gmail.com>
2019-10-24 00:18:16 -0400

    fixed something

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const cassandra = require('cassandra-driver');
+const formidable = require('formidable')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,31 +9,48 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/deposit', async function(req, res, next) {
-  let filename = req.body.filename
 
-  console.log(req)
-  console.log(filename)
-  console.log(req.body)
+  var form = new formidable.IncomingForm();
 
-  const client = new cassandra.Client({ 
-    contactPoints: ['127.0.0.1'], 
-    keyspace: 'hw6',
-    localDataCenter: 'datacenter1'
+  form.parse(req, function(err, fields, files) {
+    if (err)
+      console.log(err)
+    // Copy file from temporary place
+    // var fs = require('fs');
+    // fs.rename(file.path, <targetPath>, function (err) { ... }); 
+      
+      
+
+      // Send result on client
+    res.end(util.inspect({fields: fields, files: files}));
+    
+    console.log(fields)
+    console.log(files)
+
+
+    let filename = req.body.filename
+
+    const client = new cassandra.Client({ 
+      contactPoints: ['127.0.0.1'], 
+      keyspace: 'hw6',
+      localDataCenter: 'datacenter1'
+    });
+
+    let contents = req.files
+
+    let query = 'INSERT INTO imgs (filename, contents)'
+    let params = [filename, contents]
+
+    try {
+      await client.execute(query, params)
+    } 
+    catch (error) {
+      console.log(error)
+    }
+
+    res.status(200)
   });
 
-  let contents = req.files
-
-  let query = 'INSERT INTO imgs (filename, contents)'
-  let params = [filename, contents]
-
-  try {
-    await client.execute(query, params)
-  } 
-  catch (error) {
-    console.log(error)
-  }
-
-  res.status(200)
 });
 
 router.get('/retrieve', function(req, res, next) {
